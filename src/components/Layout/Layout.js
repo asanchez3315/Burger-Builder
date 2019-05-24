698e516811df584346163f2d6505bcb1c10d85ad
Alexander Sanchez <asanchez3315@gmail.com>
2019-05-24 14:20:45 -0400

    uploading app

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import Auxillary from '../../hoc/Auxillary';
+import classes from './Layout.module.css';
+
+const layout = (props) => (
+
+    <Auxillary>
+        <div> Toolbar, Side Drawer, Backdrop</div>
+        <main className={classes.Content}>
+            {props.children}
+        </main>
+
+    </Auxillary>
+
+
+)
+
+
+
+export default layout
