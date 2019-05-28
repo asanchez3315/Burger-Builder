7eb8b7b56c9b91ea0a9c0f54d3364a119da05812
Alexander Sanchez <asanchez3315@gmail.com>
2019-05-28 17:40:22 -0400

    added hamburger icon

--- src/components/togglebtn/ToggleBtn.js
+++ src/components/togglebtn/ToggleBtn.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import classes from './toggleBtn.module.css'
 
 const toggleBtn = (props) => (
 
-    <button onClick={props.clicked}>Menu</button>
+    <div className={classes.DrawerToggle} onClick={props.clicked}>
+        <div></div>
+        <div></div>
+        <div></div>
+
+    </div>
 )
 
 export default toggleBtn
