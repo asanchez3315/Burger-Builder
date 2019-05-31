093f9d61edd89c36871f2e95691c1b2a2b1bd45c
Alexander Sanchez <asanchez3315@gmail.com>
2019-05-31 17:07:13 -0400

    added error handling

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,8 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal'
 import OrderSummary from '../../components/UI/OrderSummary/OrderSummary'
 import axios from '../../axiosOrders'
+import Spinner from '../../components/UI/Spinner/Spinner'
+import ErrorHandler from '../../hoc/ErrorHandler/ErrorHandler'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -25,7 +27,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false,
-        purchasing:false
+        purchasing: false,
+        loading: false
     }
 
     updatedPurchaseState(ingredients) {
@@ -39,39 +42,39 @@ class BurgerBuilder extends Component {
         this.setState({ purchasable: sum > 0 })
     }
 
-purchasingHandler = () =>{
-    this.setState({purchasing:true})
-}
-
-purchasingCancelHandler = () =>{
-    this.setState({purchasing:false})
-}
-purchasingContinuelHandler = () =>{
-    const order = {
-       ingredients: this.state.ingredients,
-       price: this.state.price,
-       Customer:{
-        Name:'Alexander Saanchez',
-        address:{
-            street:'testStreet12',
-            city: 'Orlando',
-            Zipcode: '23456',
-            country:'USA'
-        },
-
-        email:'test@test.com',
-        delivery:'fastest'
-       } 
+    purchasingHandler = () => {
+        this.setState({ purchasing: true })
     }
-    // alert('you continued!')
-    axios.post('/orders.json',order)
-    .then(response =>{
-        console.log(response)
-        .catch(error =>{
-            console.log(error)
-        })
-    })
-}
+
+    purchasingCancelHandler = () => {
+        this.setState({ purchasing: false })
+    }
+    purchasingContinuelHandler = () => {
+        this.setState({loading:true})
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.price,
+            Customer: {
+                Name: 'Alexander Sanchez',
+                address: {
+                    street: 'testStreet12',
+                    city: 'Orlando',
+                    Zipcode: '23456',
+                    country: 'USA'
+                },
+
+                email: 'test@test.com',
+                delivery: 'fastest'
+            }
+        }
+        // alert('you continued!')
+        axios.post('/orders.json', order)
+            .then(response => {
+                this.setState({loading:false, purchasing: false})
+            }).catch(error => {
+                this.setState({loading:false, purchasing: false})
+            })
+    }
 
     addIngredientHandler = (type) => {
         const oldCount = this.state.ingredients[type];
@@ -116,17 +119,25 @@ purchasingContinuelHandler = () =>{
         for (let key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0
         }
+        let orderSummary = <OrderSummary ingredients={this.state.ingredients}
+        purchaseCancelled={this.purchasingCancelHandler}
+        purchaseContinue={this.purchasingContinuelHandler}
+        price={this.state.totalPrice} />
+
+        if(this.state.loading){
+
+          orderSummary = <Spinner />
+
+        }
+
 
 
         return (
 
             <Auxillary>
 
-                <Modal  show={this.state.purchasing} modalClosed={this.purchasingCancelHandler} > 
-                    <OrderSummary ingredients={this.state.ingredients}
-                     purchaseCancelled={this.purchasingCancelHandler}
-                     purchaseContinue={this.purchasingContinuelHandler}
-                     price={this.state.totalPrice}/>
+                <Modal show={this.state.purchasing} modalClosed={this.purchasingCancelHandler} >
+                    {orderSummary}
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
@@ -146,4 +157,4 @@ purchasingContinuelHandler = () =>{
 
 
 
-export default BurgerBuilder;
+export default ErrorHandler(BurgerBuilder,axios);
