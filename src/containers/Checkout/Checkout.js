4c0c2957582546cc3c158be01eb03a5ca0f5b187
Alexander Sanchez <asanchez3315@gmail.com>
2019-06-09 00:33:05 -0400

    passed ingredients via queries

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -3,31 +3,44 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary'
 
 class Checkout extends Component {
 
-    state={
-        ingredients:{
-            salad:1,
-            cheese:1,
-            bacon:1,
-            meat:1
+    state = {
+        ingredients: {
+            salad: 1,
+            cheese: 1,
+            bacon: 1,
+            meat: 1
         }
     }
 
-    checkoutCancelledHandler = () =>{
-this.props.history.goBack();
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search)
+        const ingredients = {}
+        for (let param of query.entries()) {
+            // [salad,1]
+            ingredients[param[0]] = +param[1]
+        }
+
+        this.setState({ingredients:ingredients})
     }
 
-    checkoutContinuedHandler = () =>{
+    checkoutCancelledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler = () => {
         this.props.history.replace('/checkout/checkout-data');
     }
+
+
 
     render() {
         return (
             <div>
-                <CheckoutSummary 
-                ingredients ={this.state.ingredients}
-                checkoutCancelled={this.checkoutCancelledHandler}
-                checkoutContinued={this.checkoutContinuedHandler}
-                 />
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutCancelled={this.checkoutCancelledHandler}
+                    checkoutContinued={this.checkoutContinuedHandler}
+                />
 
             </div>
         )
