0604c47814a21ac67910d6b1e68ee2e6b5100e26
Alexander Sanchez <asanchez3315@gmail.com>
2019-06-08 23:36:34 -0400

    added handlers to checkout summary

--- src/components/Order/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary.js
@@ -11,8 +11,8 @@ const checkoutSummary = (props) => {
             <div style={{ width: '100%',  }}>
                     <Burger ingredients={props.ingredients} />
             </div>
-            <Button clicked btnType='Danger'>Cancel</Button>
-            <Button clicked btnType='Success'>Continue</Button>
+            <Button  clicked={props.checkoutCancelled} btnType='Danger'>Cancel</Button>
+            <Button clicked={props.checkoutContinue} btnType='Success'>Continue</Button>
 
         </div>
 
